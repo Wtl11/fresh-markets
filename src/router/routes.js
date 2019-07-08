c74b4f712e6530ae879e19a1163e882901d5d471
jtr <jtr354@foxmail.com>
2019-07-08 16:20:27 +0800

    测试分支init

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,30 +1,32 @@
 export default [
-  // 测试页面配置
   {
-    path: '/test-page1',
-    name: 'test-page1',
-    component: () => lazyLoadView(import('@pages/test-page1/test-page1'))
-  },
-  // 测试页面注释
-  {
-    path: '/test-page',
-    name: 'test-page',
-    component: () => lazyLoadView(import('@pages/test-page/test-page'))
-  },
-  {
-    path: '/',
+    path: '/home',
     name: 'home',
-    component: () => lazyLoadView(import('@pages/home/home'))
-  },
-  {
-    path: '/sample',
-    name: 'sample',
-    component: () => lazyLoadView(import('@pages/sample/sample'))
-  },
-  {
-    path: '/other-pages',
-    name: 'other-pages',
-    component: () => lazyLoadView(import('@pages/other-pages/other-pages'))
+    component: () => import('@pages/home/home'),
+    children: [
+      // 测试页面配置
+      {
+        path: '/test-page1',
+        name: 'test-page1',
+        component: () => import('@pages/test-page1/test-page1')
+      },
+      // 测试页面注释
+      {
+        path: '/test-page',
+        name: 'test-page',
+        component: () => import('@pages/test-page/test-page')
+      },
+      {
+        path: '/sample',
+        name: 'sample',
+        component: () => import('@pages/sample/sample')
+      },
+      {
+        path: '/other-pages',
+        name: 'other-pages',
+        component: () => import('@pages/other-pages/other-pages')
+      },
+    ]
   },
   {
     path: '/404',
@@ -38,20 +40,20 @@ export default [
   }
 ]
 
-function lazyLoadView(AsyncView) {
-  const AsyncHandler = () => ({
-    component: AsyncView,
-    loading: require('@pages/_loading/_loading').default,
-    delay: 400,
-    error: require('@pages/_timeout/_timeout').default,
-    timeout: 10000
-  })
-
-  return Promise.resolve({
-    functional: true,
-    render(h, {data, children}) {
-      // 将属性和方法传递给所有展示组件
-      return h(AsyncHandler, data, children)
-    }
-  })
-}
+// function lazyLoadView(AsyncView) {
+//   const AsyncHandler = () => ({
+//     component: AsyncView,
+//     loading: require('@pages/_loading/_loading').default,
+//     delay: 400,
+//     error: require('@pages/_timeout/_timeout').default,
+//     timeout: 10000
+//   })
+//
+//   return Promise.resolve({
+//     functional: true,
+//     render(h, {data, children}) {
+//       // 将属性和方法传递给所有展示组件
+//       return h(AsyncHandler, data, children)
+//     }
+//   })
+// }
