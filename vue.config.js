e019fb2a05a57985341013d620c1e9cf8f8b1ded
jtr <jtr354@foxmail.com>
2019-07-12 11:52:20 +0800

    修改跳转

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const appConfig = require('./src/app.config')
+const TerserPlugin = require('terser-webpack-plugin');
+
 
 module.exports = {
   // baseUrl: process.env.VUE_APP_ENV !== 'production' ? './' : '/',
@@ -6,11 +8,64 @@ module.exports = {
     name: appConfig.title,
     resolve: {
       alias: require('./aliases.config').webpack
-    }
+    },
+    optimization: {
+      concatenateModules: true,
+      minimizer: [
+        new TerserPlugin({
+          include: /src/,
+          test: /\.js(\?.*)?$/i,
+          parallel: true
+        }),
+      ],
+      runtimeChunk: {
+        name: 'manifest',
+      },
+      splitChunks: {
+        cacheGroups: {
+          vendors: {
+            name: 'chunk-vendors',
+            test: /node_modules/,
+            priority: -10,
+            chunks: 'initial',
+            minChunks: 1,
+            maxInitialRequests: 5,
+            minSize: 1024 * 244,
+            maxSize: 1024 * 1024 / 2
+          },
+          common: {
+            name: 'chunk-common',
+            minChunks: 2,
+            priority: -20,
+            chunks: 'initial',
+            reuseExistingChunk: true,
+            maxInitialRequests: 5,
+            minSize: 1024 * 244,
+            maxSize: 1024 * 1024 / 2
+          },
+        }
+      }
+    },
+  },
+  chainWebpack(config) {
+    config.plugins.delete('prefetch')
+    config.module
+    .rule('images')
+    .use('image-webpack-loader')
+    .loader('image-webpack-loader')
+    .options({
+      bypassOnDebug: true
+    })
+    .end()
+    return config
   },
   css: {
-    sourceMap: true
+    sourceMap: false,
+    extract: true,
+    loaderOptions: {},
+    modules: false
   },
+  productionSourceMap: false,
   devServer: {
     port: 8956,
     ...(process.env.VUE_APP_API
